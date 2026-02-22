b0f29e98594941a79ff44313f59a3a99cccc5cfd
maxvilhenaviana-maker <maxvilhenaviana@gmail.com>
2026-02-22 17:41:10 -0300

    Add files via upload

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,40 @@
+import axios from 'axios';
 
+const api = axios.create({
+  baseURL: '/api'
+});
+
+api.interceptors.request.use((config) => {
+  const tokens = JSON.parse(localStorage.getItem('auth_tokens'));
+  if (tokens?.accessToken) {
+    config.headers.Authorization = `Bearer ${tokens.accessToken}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const tokens = JSON.parse(localStorage.getItem('auth_tokens'));
+        const { data } = await axios.post('/api/auth/refresh', { refreshToken: tokens.refreshToken });
+        
+        const newAuth = { ...tokens, accessToken: data.accessToken };
+        localStorage.setItem('auth_tokens', JSON.stringify(newAuth));
+        
+        originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
+        return api(originalRequest);
+      } catch (err) {
+        localStorage.removeItem('auth_tokens');
+        window.location.href = '/login';
+        return Promise.reject(err);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
