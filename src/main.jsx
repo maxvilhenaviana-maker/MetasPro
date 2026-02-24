ba3b60b1aa2a1680fc88de26b5c710819687b9dc
maxvilhenaviana-maker <maxvilhenaviana@gmail.com>
2026-02-23 22:28:11 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx';
 
-// Este arquivo é a ponte entre o seu código React (App.jsx) 
-// e o elemento <div id="root"></div> que está no seu index.html
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
